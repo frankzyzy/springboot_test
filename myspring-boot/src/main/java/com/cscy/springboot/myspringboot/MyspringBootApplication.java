15f1190996f533813cc4fd09809134e30777ba17
yzengchn <yzengchn@163.com>
2018-08-17 21:55:06 +0800

    加入

--- myspring-boot/src/main/java/com/cscy/springboot/myspringboot/MyspringBootApplication.java
+++ myspring-boot/src/main/java/com/cscy/springboot/myspringboot/MyspringBootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyspringBootApplication {
-//sasasas
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringBootApplication.class, args);
 	}
